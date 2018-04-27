e92feb5cbf57212c0f77afbd3bb528ddb70561c3
David Osama <david_u_riad@hotmail.com>
2018-04-27 14:49:57 +0200

    Initial commit

---
+++ PewChatClient/src/pewchatclient/PewChatClient.java
@@ -0,0 +1,12 @@
+package pewchatclient;
+
+public class PewChatClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
---
+++ PewChatServer/src/pewchatserver/PewChatServer.java
@@ -0,0 +1,12 @@
+package pewchatserver;
+
+public class PewChatServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
