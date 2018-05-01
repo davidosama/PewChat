8ad4887cdb114a5da9ce2fadd4a10c4c377e5a99
ToniGeorge <tonigeorge@outlook.com>
2018-05-01 20:08:21 +0200

    Revert "Revert "createGroups under construction""
    This reverts commit d45ee7abfd49abc58a80ef02ae96557b688b9849.

---
+++ PewChatServer/src/pewchatserver/Group.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pewchatserver;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author minarafla
+ */
+public class Group {
+    
+    StringBuffer GroupName;
+    StringBuffer Messages;
+    ArrayList<User> Participants;
+    User Admin;
+    static ArrayList<String> AllGroupsNames;
+    
+    public Group(String GroupName, User Admin){
+        
+        this.GroupName=new StringBuffer(GroupName);
+        Participants=new ArrayList<User>();
+        Messages = new StringBuffer();
+        this.Admin=Admin;
+        AllGroupsNames.add(GroupName);
+        
+   }
+    
+    public void addParticipant(User newParticipant){
+       Participants.add(newParticipant);
+    }
+    
+    
+}
